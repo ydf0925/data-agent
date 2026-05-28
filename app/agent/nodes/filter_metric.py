eca05be9ac69d241c3a2fd11b65fbbff115545d9
ydf <y1134382928@gmail.com>
2026-05-28 22:52:04 +0800

    过滤信息

--- app/agent/nodes/filter_metric.py
+++ app/agent/nodes/filter_metric.py
@@ -1,11 +1,30 @@
+import yaml
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
 from langgraph.runtime import Runtime
 
 from app.agent.context import DataAgentContext
-from app.agent.state import DataAgentState
+from app.agent.llm import llm
+from app.agent.state import DataAgentState, MetricInfoState
+from app.prompt.prompt_loader import load_prompt
+from app.core.log import logger
 
 
 async def filter_metric(state: DataAgentState, runtime: Runtime[DataAgentContext]):
     writer = runtime.stream_writer
     writer("过滤指标信息")
-    import asyncio
-    await asyncio.sleep(0.5)
+
+    query = state["query"]
+    metric_infos: list[MetricInfoState] = state["metric_infos"]
+
+    prompt = PromptTemplate(template=load_prompt("filter_metric_info"), input_variables=["query", "metric_infos"])
+    output_parser = JsonOutputParser()
+    chain = prompt | llm | output_parser
+
+    result = await chain.ainvoke({"query": query,
+                                  "metric_infos": yaml.dump(metric_infos, allow_unicode=True, sort_keys=False)})
+    filtered_metric_infos = [metric_info for metric_info in metric_infos if metric_info["name"] in result]
+
+    logger.info(f"过滤后的指标信息{[filtered_metric_info["name"] for filtered_metric_info in filtered_metric_infos]}")
+
+    return {"metric_infos": filtered_metric_infos}
--- app/agent/nodes/filter_table.py
+++ app/agent/nodes/filter_table.py
@@ -1,11 +1,33 @@
+import yaml
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
 from langgraph.runtime import Runtime
 
 from app.agent.context import DataAgentContext
-from app.agent.state import DataAgentState
+from app.agent.llm import llm
+from app.agent.state import DataAgentState, TableInfoState
+from app.prompt.prompt_loader import load_prompt
+from app.core.log import logger
 
 
 async def filter_table(state: DataAgentState, runtime: Runtime[DataAgentContext]):
     writer = runtime.stream_writer
     writer("过滤表信息")
-    import asyncio
-    await asyncio.sleep(0.5)
+
+    query = state["query"]
+    table_infos: list[TableInfoState] = state["table_infos"]
+
+    prompt = PromptTemplate(template=load_prompt("filter_table_info"), input_variables=["query", "table_infos"])
+    output_parser = JsonOutputParser()
+    chain = prompt | llm | output_parser
+    result = await chain.ainvoke({"query": query,
+                                  "table_infos": yaml.dump(table_infos, allow_unicode=True, sort_keys=False)})
+    filtered_table_infos: list[TableInfoState] = []
+    for table_info in table_infos:
+        if table_info["name"] in result:
+            table_info["columns"] = [column_info for column_info in table_info["columns"] if
+                                     column_info["name"] in result[table_info["name"]]]
+            filtered_table_infos.append(table_info)
+    logger.info(f"过滤后的表信息{[filtered_table_info for filtered_table_info in filtered_table_infos]}")
+
+    return {"table_infos": filtered_table_infos}
